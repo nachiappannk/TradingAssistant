2b57d2b1b6f2cc318d5c2dc990fec8d9bdc5f426
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-03 18:38:34 +0530

    Clean up

--- TradingAssistant/TradingAssistantViewModel/Logger.cs
+++ TradingAssistant/TradingAssistantViewModel/Logger.cs
@@ -6,14 +6,8 @@ namespace Nachiappan.TradingAssistantViewModel
     public class Logger : ILogger
     {
 
-        public List<Information> InformationList = new List<Information>();
         public void Log(MessageType type, string message)
         {
-            if (type == MessageType.Warning)
-                InformationList.Add(Information.CreateWarning(message));
-
-            if (type == MessageType.Error)
-                InformationList.Add(Information.CreateError(message));
         }
     }
 }
